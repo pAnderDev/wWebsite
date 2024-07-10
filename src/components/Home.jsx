6a2eb5be3cb66fddf9ec075c1bf928e76cab4eec
pAnderDev <patrick@casaanderson.net>
2024-07-10 16:18:25 -0500

    ci/cd deploy

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Williams Website</h1>
+      <p>Professional bookkeeping services to help you manage your finances efficiently.</p>
+    </div>
+  );
+};
+
+export default Home;
