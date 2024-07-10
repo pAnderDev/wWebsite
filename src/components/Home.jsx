84ba4ff65555a8d25b2e1ad012ef90f30dbd0f4b
pAnderDev <patrick@casaanderson.net>
2024-07-10 16:25:02 -0500

    modified:   src/components/Home.jsx 	modified:   src/index.js

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     <div>
       <h1>Welcome to Williams Website</h1>
       <p>Professional bookkeeping services to help you manage your finances efficiently.</p>
+      <p>TEST THE CI/CD integration</p>
     </div>
   );
 };
