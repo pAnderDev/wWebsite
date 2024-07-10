9b255754da4e13fee1084e29ad0c52dd46179b43
pAnderDev <patrick@casaanderson.net>
2024-07-10 16:35:29 -0500

    modified:   src/components/Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,6 @@ const Home = () => {
     <div>
       <h1>Welcome to Williams Website</h1>
       <p>Professional bookkeeping services to help you manage your finances efficiently.</p>
-      <p>TEST THE CI/CD integration</p>
-      <p>TEST REMOVING THE AWS AMPLIFY CATEGIRY</p>
     </div>
   );
 };
